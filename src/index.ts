6bfca83466aefab156507567b15ed8d45a1e6bc8
Nora Rodriguez <gia.drago4@gmail.com>
2023-09-17 23:00:21 -0600

    apollo server set up

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,28 @@ import { MikroORM } from "@mikro-orm/core"
 // import { Post } from "./entities/Post";
 import mikroConfig from './mikro-orm.config'
 import express from 'express';
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from 'type-graphql'
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async() => {
     const orm = await MikroORM.init(mikroConfig);
-    const em = orm.em.fork();
+    // const em = orm.em.fork();
     await orm.getMigrator().up();
     
     const app = express();
+    
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    })
+
+    await apolloServer.start();
+    
+    apolloServer.applyMiddleware({app})
+
     app.listen(4000, () => {
         console.log('server started on local:host:4000')
     })
