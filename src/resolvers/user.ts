e69a2202a4c37bc959f2a8d2c325571591ebfe25
Nora Rodriguez <gia.drago4@gmail.com>
2023-09-22 23:24:23 -0600

    user resolver querys muted

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -104,19 +104,19 @@ export class UserResolver {
     }
 
 
-    @Query(() => [User])
-    users(
-        @Ctx() { em }:MyContext
-    ): Promise<User[]> {
-        return em.find(User, {} );
-    }
+    // @Query(() => [User])
+    // users(
+    //     @Ctx() { em }:MyContext
+    // ): Promise<User[]> {
+    //     return em.find(User, {} );
+    // }
 
-    @Query(() => User, { nullable: true })
-    user(
-        @Arg("id", () => Int) id: number,
-        @Ctx() { em }:MyContext
-    ): Promise<User | null> {
-        return em.findOne(User, { id } )
-    }
+    // @Query(() => User, { nullable: true })
+    // user(
+    //     @Arg("id", () => Int) id: number,
+    //     @Ctx() { em }:MyContext
+    // ): Promise<User | null> {
+    //     return em.findOne(User, { id } )
+    // }
 }
 
