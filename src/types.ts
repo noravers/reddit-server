d40d0a28a19f17ce6de8ecefbeec3aff78bf8a60
Nora Rodriguez <gia.drago4@gmail.com>
2023-10-01 18:37:55 -0600

    types augmentation to SessionData form express-session

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,15 @@
 import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core"
-import { Request, Response } from 'express'
+import { Request, Response } from 'express';
+// import session from 'express-session'
+
+declare module 'express-session' {
+    interface SessionData {
+        userId: number
+    }
+}
 
 export type MyContext = {
     em: EntityManager<IDatabaseDriver<Connection>>;
-    req: Request;
+    req: Request
     res: Response;
 }
