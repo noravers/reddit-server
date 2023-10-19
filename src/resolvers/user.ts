c941e8e3b30a2feee787fd2ab32caf9393f7d72c
Nora Rodriguez <gia.drago4@gmail.com>
2023-10-18 22:15:12 -0600

    .env config

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -120,6 +120,7 @@ export class UserResolver {
         }        
                 
         req.session!.userId = user.id
+        console.log(req.session)
      
         return {
             user
